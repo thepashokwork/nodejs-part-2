be434ee8672630bd7552aced63ead5cdc14b2109
Pavel Tseluiko <pavel.tseluiko@gmail.com>
2024-08-20 19:42:48 +0300

    Add promises

--- file-system-lesson-11/main.js
+++ file-system-lesson-11/main.js
@@ -4,6 +4,33 @@ const PATH = './example.txt';
 
 const readStream = fs.createReadStream(PATH);
 
+// Using of Promises
+function readFileWithPromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./example.txt', 'utf8', (error, data) => {
+      /*
+      If file doesn't exists then the log output would be:
+
+      [Error: ENOENT: no such file or directory, open './example1.txt'] {
+        errno: -2,
+        code: 'ENOENT',
+        syscall: 'open',
+        path: './example1.txt'
+      }
+       */
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(data);
+    });
+  });
+}
+
+readFileWithPromise()
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
 // Using of Callback
 const handleReadFileData = (err, data) => {
   if (err) {
@@ -16,7 +43,7 @@ const handleReadFileData = (err, data) => {
   // <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21>
 
   // Logs: "Hello World!" if calling a method "toString()"
-  console.log(data.toString());
+  // console.log(data.toString());
 }
 
 function readFileData() {
@@ -26,71 +53,71 @@ function readFileData() {
 
 readFileData();
 
-const readFile = fs.readFile
-
-function readFileSync() {
-  try {
-    const content = fs.readFileSync(PATH, 'utf8');
-    // logs: Hello World!
-    console.log(content);
-  }
-  catch (error) {
-    // Error: ENOENT: no such file or directory, open './example1.txt'
-    console.log(error);
-  }
-}
-
-readFileSync();
-
-function getFolderFiles() {
-  // Logs: Files:  [ 'example.txt', 'main.js' ]
-  try {
-    const files = fs.readdirSync('./', 'utf8');
-    console.log('Files: ', files);
-  }
-  catch (error) {
-    console.log('Error: ', error);
-  }
-}
-
-getFolderFiles();
-
-function createReadStream() {
-  readStream
-    .on('data', (data) => {
-      // Creates a buffer
-      // logs: data <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21>
-      console.log('data', data);
-      // logs: data Hello World!
-      console.log('data', data.toString());
-    })
-    .on('error', (error) => {
-      // Logs an error
-      console.log('error', error);
-    });
-}
-
-createReadStream();
-
-function createAndUpdateFile() {
-  // Createa a new file "example1.txt" with a new data
-  try {
-    fs.writeFileSync('./example1.txt', 'New Hello World!', 'utf8');
-  }
-  catch (error) {
-    console.log('Error:', error);
-  }
-
-  // Updates already existing file
-  try {
-    fs.appendFileSync('./example.txt', '\n Some new Data', 'utf8');
-  }
-  catch (error) {
-    console.log('Error:', error);
-  }
-}
-
-createAndUpdateFile();
+// const readFile = fs.readFile
+//
+// function readFileSync() {
+//   try {
+//     const content = fs.readFileSync(PATH, 'utf8');
+//     // logs: Hello World!
+//     console.log(content);
+//   }
+//   catch (error) {
+//     // Error: ENOENT: no such file or directory, open './example1.txt'
+//     console.log(error);
+//   }
+// }
+//
+// readFileSync();
+//
+// function getFolderFiles() {
+//   // Logs: Files:  [ 'example.txt', 'main.js' ]
+//   try {
+//     const files = fs.readdirSync('./', 'utf8');
+//     console.log('Files: ', files);
+//   }
+//   catch (error) {
+//     console.log('Error: ', error);
+//   }
+// }
+//
+// getFolderFiles();
+//
+// function createReadStream() {
+//   readStream
+//     .on('data', (data) => {
+//       // Creates a buffer
+//       // logs: data <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21>
+//       console.log('data', data);
+//       // logs: data Hello World!
+//       console.log('data', data.toString());
+//     })
+//     .on('error', (error) => {
+//       // Logs an error
+//       console.log('error', error);
+//     });
+// }
+//
+// createReadStream();
+//
+// function createAndUpdateFile() {
+//   // Createa a new file "example1.txt" with a new data
+//   try {
+//     fs.writeFileSync('./example1.txt', 'New Hello World!', 'utf8');
+//   }
+//   catch (error) {
+//     console.log('Error:', error);
+//   }
+//
+//   // Updates already existing file
+//   try {
+//     fs.appendFileSync('./example.txt', '\n Some new Data', 'utf8');
+//   }
+//   catch (error) {
+//     console.log('Error:', error);
+//   }
+// }
+//
+// createAndUpdateFile();
 
 // Create a new directory
 // fs.mkdirSync('./docs/files', {
