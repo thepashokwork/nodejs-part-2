d39a7000574907c025d055d51cb7e1e56af4eeab
Pavel Tseluiko <pavel.tseluiko@gmail.com>
2024-08-25 14:00:45 +0300

    INitiate lesson 14

---
+++ hw-lesson-14/main.ts
@@ -0,0 +1,17 @@
+import { Options } from './options.interface.js';
+
+/**
+ * Encodes content to base64 and creates a new file with this encoded text.
+ *
+ * @param pathToFile
+ * @param options
+ */
+function encodeToBase64(pathToFile: string, options?: Options): void {
+
+}
+
+const options: Options = {
+  replace: 'ddd',
+};
+
+console.log('test', options);
